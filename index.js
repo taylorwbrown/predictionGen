7a666d74909d3038ce55d9347aaf9c1703cfe555
team <team@climbsoillinc.com>
2021-12-11 14:43:17 -0600

    starting of making arrays

---
+++ index.js
@@ -0,0 +1,14 @@
+// Creating the empty array that will show the statement.
+const SentenceArray = [];
+
+// Creating the arrays for the three sentence sections. Held within an object.
+const randoArrays = {
+    name: ['Steve', 'Rackus', 'Bean', 'Boinkus', 'Cheese', 'Carlton'],
+  
+    feeling: ['is upset.', 'wants beans.', 'despises you.', 'wants bread.', 'loves you.', 'takes pity.'],
+  
+    
+  
+  }
+
+
