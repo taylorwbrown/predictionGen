5bf6ad307798ff1eecd9ab5e3f0cc91512f964c0
Taylor Brown <taylorbrown131@yahoo.com>
2021-12-12 23:47:56 -0600

    completed program

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 // Creating the arrays for the three sentence sections. Held within an object.
 const randoArrays = {
-    
-   // Creating the empty array that will show the statement.
-    sentenceArray: [],
 
     name: ['Steve', 'Rackus', 'Bean', 'Boinkus', 'Cheese', 'Carlton'],
   
@@ -10,8 +7,24 @@ const randoArrays = {
   
     statement: ['Will require attention.', 'Give beans.', 'Give cheese.', 'Take to dinner.', 'Bing Bong.', 'Run!', 'Love them forever.'],
 
-    
+    // sets up randomized index numbers to be called on later.
+    randomName() {
+      return this.name(Math.floor(Math.random() * this.name.length()));
+    },
 
-  }
+    randomFeeling() {
+      return this.feeling(Math.floor(Math.random() * this.feeling.length()));
+    },
 
+    randomStatement() {
+      return this.statement(Math.floor(Math.random() * this.statement.length()));
+    },
 
+    // Puts the three randomizations together.
+    makeSentence() {
+      console.log(`${this.randomName} ${this.randomFeeling} ${this.randomStatement}`);
+    }
+
+  };
+
+randoArrays.makeSentence;
