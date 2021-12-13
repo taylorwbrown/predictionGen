fd8d9c86c395aafaacd6dc31202ad2f9f463c85e
Taylor Brown <taylorbrown131@yahoo.com>
2021-12-12 23:34:48 -0600

    finished arrays

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-// Creating the empty array that will show the statement.
-const SentenceArray = [];
-
 // Creating the arrays for the three sentence sections. Held within an object.
 const randoArrays = {
+    
+   // Creating the empty array that will show the statement.
+    sentenceArray: [],
+
     name: ['Steve', 'Rackus', 'Bean', 'Boinkus', 'Cheese', 'Carlton'],
   
     feeling: ['is upset.', 'wants beans.', 'despises you.', 'wants bread.', 'loves you.', 'takes pity.'],
   
+    statement: ['Will require attention.', 'Give beans.', 'Give cheese.', 'Take to dinner.', 'Bing Bong.', 'Run!', 'Love them forever.'],
+
     
-  
+
   }
 
 
